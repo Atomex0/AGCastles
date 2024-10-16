339f2b3279de74cc02ad9a29d20b82ac9f34998e
Atomex0 <hunterr.21065@waitarahs.school.nz>
2024-10-17 10:53:16 +1300

    Final

---
+++ js/script.js
@@ -0,0 +1,99 @@
+/* NAV LINKS */
+	function HOMEYELLOW() {
+		document.getElementById("iconsh").src = "images/housei-final-yellow.png";
+		document.getElementById("icontexth").style.color = "#fbde01";
+	}
+	function HOME() {
+		document.getElementById("iconsh").src = "images/housei-final.png";
+		document.getElementById("icontexth").style.color = "#4a91df";
+	}
+	function EQUIPMENTYELLOW() {
+		document.getElementById("iconsi").src = "images/infoi-final-yellow.png";
+		document.getElementById("icontexte").style.color = "#fbde01";
+	}
+	function EQUIPMENT() {
+		document.getElementById("iconsi").src = "images/infoi-final.png";
+		document.getElementById("icontexte").style.color = "#4a91df";
+	}
+	function CONTACTYELLOW() {
+		document.getElementById("iconsp").src = "images/phonei-final-yellow.png";
+		document.getElementById("icontextc").style.color = "#fbde01";
+	}
+	function CONTACT() {
+		document.getElementById("iconsp").src = "images/phonei-final.png";
+		document.getElementById("icontextc").style.color = "#4a91df";
+	}
+/* BURGER MENU */
+	function DROPBORDER() {
+		document.getElementById("dropdown").setAttribute("style","border-bottom: 8px solid #4a91df");
+	}
+	function BURGER() {
+		document.getElementById("dropdown").style.height = "259px";
+	}
+	function CLOSEBORDER() {
+		document.getElementById("dropdown").setAttribute("style","0");
+	}
+	function CLOSE() {
+		document.getElementById("dropdown").style.height = "0";
+	}
+/* GRID DROPDOWN */
+	function GRIDOPEN1() {
+		document.getElementById("griddrop1").style.height = "190px";
+	}
+	function GRIDCLOSE1() {
+		document.getElementById("griddrop1").style.height = "0";
+	}
+	document.addEventListener("DOMContentLoaded", function () {
+		GRIDOPEN1();
+		GRIDCLOSE1();
+	});
+	function GRIDOPEN2() {
+		document.getElementById("griddrop2").style.height = "190px";
+	}
+	function GRIDCLOSE2() {
+		document.getElementById("griddrop2").style.height = "0";
+	}
+	document.addEventListener("DOMContentLoaded", function () {
+		GRIDOPEN2();
+		GRIDCLOSE2();
+	});
+	function GRIDOPEN3() {
+		document.getElementById("griddrop3").style.height = "190px";
+	}
+	function GRIDCLOSE3() {
+		document.getElementById("griddrop3").style.height = "0";
+	}
+	document.addEventListener("DOMContentLoaded", function () {
+		GRIDOPEN3();
+		GRIDCLOSE3();
+	});
+	function GRIDOPEN4() {
+		document.getElementById("griddrop4").style.height = "190px";
+	}
+	function GRIDCLOSE4() {
+		document.getElementById("griddrop4").style.height = "0";
+	}
+	document.addEventListener("DOMContentLoaded", function () {
+		GRIDOPEN4();
+		GRIDCLOSE4();
+	});
+	function GRIDOPEN5() {
+		document.getElementById("griddrop5").style.height = "190px";
+	}
+	function GRIDCLOSE5() {
+		document.getElementById("griddrop5").style.height = "0";
+	}
+	document.addEventListener("DOMContentLoaded", function () {
+		GRIDOPEN5();
+		GRIDCLOSE5();
+	});
+	function GRIDOPEN6() {
+		document.getElementById("griddrop6").style.height = "190px";
+	}
+	function GRIDCLOSE6() {
+		document.getElementById("griddrop6").style.height = "0";
+	}
+	document.addEventListener("DOMContentLoaded", function () {
+		GRIDOPEN6();
+		GRIDCLOSE6();
+	});
